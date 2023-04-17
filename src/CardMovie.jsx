b35747f55e87e1c3ba24ad4f76b81e239dd5d573
™☠שєςt๏г☠™ ™☠שєςt๏г☠™ <mhcc213@gmail.com>
2023-04-17 03:38:28 +0200

    finished UI

--- src/CardMovie.jsx
+++ src/CardMovie.jsx
@@ -4,7 +4,16 @@ const CardMovie = () => {
   return (
     <div className="container">
       <div className="card">
-        <img src="" alt="" />
+        <div className="overlay">
+          <p>اسم الفيلم : Moonlight</p>
+          <p>تاريخ الاصدار : 2023-02-10</p>
+          <p>النوع : خيال علمي</p>
+          <p>التقييم : 9.7</p>
+        </div>
+        <img
+          src="https://s.yimg.com/ny/api/res/1.2/ZzAHlDHi8a2xdBRRbruaYQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTkyOA--/https://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/d05a3f087fa57f6d41b865d53a42a5f5"
+          alt=""
+        />
       </div>
     </div>
   );
