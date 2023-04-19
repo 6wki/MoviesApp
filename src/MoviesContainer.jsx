b06bc276c5809e39bbfc239e590d205a5390b7d3
™☠שєςt๏г☠™ ™☠שєςt๏г☠™ <mhcc213@gmail.com>
2023-04-19 12:35:23 +0200

    pagination added

--- src/MoviesContainer.jsx
+++ src/MoviesContainer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import CardMovie from "./CardMovie";
-import Pagination from "@mui/material/Pagination";
-import Stack from "@mui/material/Stack";
+import { Pagination } from "react-bootstrap";
+import ReactPaginate from "react-paginate";
 
 const MoviesContainer = ({ movies }) => {
+  const handlePageClick = (data) => {};
+
+  const pageCount = 500;
   return (
     <div className="moviesContainer">
-      <div className="container">
+      <div className="container1">
         {movies.length >= 1 ? (
           movies.map((mov) => {
             return <CardMovie key={mov.id} mov={mov} />;
@@ -15,9 +18,27 @@ const MoviesContainer = ({ movies }) => {
           <h2>لا يوجد أفلام</h2>
         )}
       </div>
-      <Stack spacing={2}>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="التالي >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< السابق"
+        containerClassName="pagination justify-content-center p-3"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        nextClassName="page-item"
+        previousLinkClassName="page-link"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        activeClassName="active"
+      />
+      {/* <Stack spacing={2}>
         <Pagination className="pagination" count={10} color="primary" />
-      </Stack>
+      </Stack> */}
     </div>
   );
 };
