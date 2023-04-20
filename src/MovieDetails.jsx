108b65fc02fdd890c55c8bf0c9402fef2b7bba22
™☠שєςt๏г☠™ ™☠שєςt๏г☠™ <mhcc213@gmail.com>
2023-04-20 09:25:54 +0200

    movie details page

--- src/MovieDetails.jsx
+++ src/MovieDetails.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Poster from "./assets/poster.jpg";
 
 const MovieDetails = () => {
-  return <div>هذه تفاصيل الفيلم</div>;
+  return (
+    <div className="movieDetails container1">
+      <div className="tpl">
+        <div className="top">
+          <div className="info">
+            <h3>اسم الفيلم: فيلم</h3>
+            <h3>تاريخ الفيلم: تاريخ</h3>
+            <h3>عدد المقيمين: عدد</h3>
+            <h3>التقييم: 8.5</h3>
+          </div>
+          <img src={Poster} alt="" />
+        </div>
+      </div>
+      <div className="bottom">
+        <h3>القصة</h3>
+        <p>تبسمشن مسني شمك ط شكطيمكش يكش ممن</p>
+      </div>
+      <footer>
+        <button>الرجوع الى الرئيسية</button>
+        <button>مشاهدة الفيلم</button>
+      </footer>
+    </div>
+  );
 };
 
 export default MovieDetails;
