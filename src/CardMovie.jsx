662cdddeff6e9c58edf161b3c9e0ab8eac844395
™☠שєςt๏г☠™ ™☠שєςt๏г☠™ <mhcc213@gmail.com>
2023-04-20 14:58:18 +0200

    added animation

--- src/CardMovie.jsx
+++ src/CardMovie.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 
 const CardMovie = ({ mov }) => {
   return (
     <Link to={`/movie/${mov.id}`}>
-      <div className="card">
-        <div className="overlay">
-          <p>اسم الفيلم</p>
-          <p>{mov.original_title}</p>
-          <p>تاريخ الاصدار : {mov.release_date}</p>
-          <p>النوع : خيال علمي</p>
-          <p>عدد المقيمين : {mov.vote_count}</p>
-          <p>التقييم : {mov.vote_average}</p>
+      <Fade bottom>
+        <div className="card">
+          <div className="overlay">
+            <p>اسم الفيلم</p>
+            <p>{mov.original_title}</p>
+            <p>تاريخ الاصدار : {mov.release_date}</p>
+            <p>النوع : خيال علمي</p>
+            <p>عدد المقيمين : {mov.vote_count}</p>
+            <p>التقييم : {mov.vote_average}</p>
+          </div>
+          <img
+            src={`https://image.tmdb.org/t/p/w500` + mov.poster_path}
+            alt=""
+          />
         </div>
-        <img src={`https://image.tmdb.org/t/p/w500` + mov.poster_path} alt="" />
-      </div>
+      </Fade>
     </Link>
   );
 };
--- src/MovieDetails.jsx
+++ src/MovieDetails.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import Poster from "./assets/poster.jpg";
 import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 
 const MovieDetails = () => {
   return (
     <div className="movieDetails container1">
-      <div className="tpl">
-        <div className="top">
-          <div className="info">
-            <h3>اسم الفيلم: فيلم</h3>
-            <h3>تاريخ الفيلم: تاريخ</h3>
-            <h3>عدد المقيمين: عدد</h3>
-            <h3>التقييم: 8.5</h3>
+      <Fade bottom cascade>
+        <div className="tpl">
+          <div className="top">
+            <div className="info">
+              <h3>اسم الفيلم: فيلم</h3>
+              <h3>تاريخ الفيلم: تاريخ</h3>
+              <h3>عدد المقيمين: عدد</h3>
+              <h3>التقييم: 8.5</h3>
+            </div>
+            <img src={Poster} alt="" />
           </div>
-          <img src={Poster} alt="" />
         </div>
-      </div>
-      <div className="bottom">
-        <h3>القصة</h3>
-        <p>تبسمشن مسني شمك ط شكطيمكش يكش ممن</p>
-      </div>
+        <div className="bottom">
+          <h3>القصة</h3>
+          <p>تبسمشن مسني شمك ط شكطيمكش يكش ممن</p>
+        </div>
+      </Fade>
       <footer>
         <Link to="/">
           <button>الرجوع الى الرئيسية</button>
